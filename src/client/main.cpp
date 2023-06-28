f052217e0705d6f5855f8e4840862f85f9df0efc
Urmas Rist <urmas@urist.ee>
2023-06-28 17:03:37 +0300

    add usage message

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
-int main() {
+const char *USAGE = 
+R""""(Usage:
+client [HOSTNAME[:PORT]] [PORT]
+)"""";
+
+int main(int argc, char *argv[]) {
+	if (argc < 2) {
+		printf("%s", USAGE);
+		return 1;
+	}
 	printf("Hello World!\n");
 	return 0;
 }
