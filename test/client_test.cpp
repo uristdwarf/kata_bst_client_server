1c65fb577d028d497cee5d279f02d9af448bd392
Urmas Rist <urmas@urist.ee>
2023-06-28 18:44:23 +0300

    add a few more tests

--- test/client_test.cpp
+++ test/client_test.cpp
@@ -6,6 +6,26 @@ TEST(ClientTests, ParseSimpleHost) {
 	char *argv[] = {(char*)"client", ip_address.data()};
 	int argc = sizeof(argv);
 	
+	// Default port will be 5510
 	auto result = parse_hostname(argc, argv);
 	EXPECT_EQ(result, ip_address + ":5510");
+}
+
+TEST(ClientTests, ParseHostWithPort) {
+	string ip_address = "127.0.0.1:1234";
+	char *argv[] = {(char*)"client", ip_address.data()};
+	int argc = sizeof(argv);
+	
+	auto result = parse_hostname(argc, argv);
+	EXPECT_EQ(result, ip_address);
+}
+
+TEST(ClientTests, ParseHostAndPort) {
+	string ip_address = "127.0.0.1";
+	string port = "6789";
+	char *argv[] = {(char*)"client", ip_address.data(), port.data()};
+	int argc = sizeof(argv);
+	
+	auto result = parse_hostname(argc, argv);
+	EXPECT_EQ(result, ip_address + ":" + port);
 }
