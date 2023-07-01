db29f845e29498e004d030b331ab3aa8f3e543ab
Urmas Rist <urmas@urist.ee>
2023-07-01 23:28:33 +0300

    add next_addr()
    This function will try to open a socket for the next address. Use this
    if connecting to the current one fails.
    
    Also add some exceptions and generally clean up the code (by removing a
    lot of this statements)

--- src/lib/socket.cpp
+++ src/lib/socket.cpp
@@ -1,6 +1,9 @@
 #include "socket.h"
+#include <cstdio>
 #include <cstring>
+#include <netdb.h>
 #include <stdexcept>
+#include <unistd.h>
 
 connection::connection(string port) {
 
@@ -20,12 +23,12 @@ connection::connection(string address, string port) {
 		fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
 		exit(254);
 	}
-	for (this->info = addresses; this->info != NULL; this->info = addresses->ai_next) {
+	for (info = addresses; info != NULL; info = addresses->ai_next) {
 		if (
-		   (this->fd = socket(
-		   	this->info->ai_family,
-		   	this->info->ai_socktype,
-		   	this->info->ai_protocol)
+		   (fd = socket(
+		   	info->ai_family,
+		   	info->ai_socktype,
+		   	info->ai_protocol)
 		   ) == -1)
 		{
 			perror("client: socket");
@@ -34,16 +37,30 @@ connection::connection(string address, string port) {
 		break;
 	}
 
-	if (this->info == NULL) {
-		fprintf(stderr, "client: failed to connect\n");
-		throw runtime_error("failed to connect");
+	if (info == NULL) {
+		fprintf(stderr, "client: failed to open socket\n");
+		throw socket_error();
 	}
 }
 
 connection::~connection() {
-	
+	freeaddrinfo(info);
+	close(fd);
 }
 
 struct addrinfo connection::get_info() {
-	return *this->info;
+	return *info;
+}
+
+struct addrinfo connection::next_addr() {
+	info = info->ai_next;
+	if (info == NULL) {
+		throw runtime_error("no more addrinfo available");
+	}
+	close(fd);
+	if ((fd = socket(info->ai_family, info->ai_socktype, info->ai_protocol)) == -1) {
+		perror("client: socket");
+		throw socket_error();
+	}
+	return *info;
 }
--- src/lib/socket.h
+++ src/lib/socket.h
@@ -1,19 +1,30 @@
 #pragma once
 
+#include <exception>
 #include <netdb.h>
 #include <string>
 
+
 using namespace std;
+
+class socket_error : exception {
+	virtual const char* what() {
+		return "failed to open socket";
+	}
+};
 // Represents a socket connection. No connection is actually made
 // and the server/client are responsible for actually connecting
 // and listening/writing
 class connection {
+	// Information about remote peer;
 	struct addrinfo *info;
 
 	protected:
 	// File descriptor
 	int fd;
-	// Information about remote peer;
+	// Close socket, get next address and open socket for that.
+	// Throws exception if next addrinfo is NULL.
+	addrinfo next_addr();
 
 	public:
 	// Create connection on own host
