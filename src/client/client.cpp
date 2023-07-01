691997b7ca34774dcf04da0c65436b9ac04f3bc8
Urmas Rist <urmas@urist.ee>
2023-07-01 23:31:22 +0300

    implement client class
    Also add various exceptions

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -1,7 +1,16 @@
 #include "client.h"
+#include <sys/socket.h>
+
+#define DATA_SIZE 1000
 
 client::client(string ip_address, string port) : connection(ip_address, port) {
-		
+	auto info = get_info();
+	while (connect(fd, info.ai_addr, info.ai_addrlen) == -1) {
+		perror("connect");
+		try {
+			next_addr();
+		} catch (socket_error) {} // socket might not work on this address
+	}
 }
 
 client::~client() {
@@ -9,9 +18,22 @@ client::~client() {
 }
 
 void client::send(string message) {
-
+	int sent = ::send(fd, message.data(), message.length(), 0);
+	// printf("client: %s", message.data());
+	if (sent == -1) {
+		// TODO: Handle server side closures
+		throw send_err();
+	} else if (sent != 0) {
+		send(message.substr(sent));
+	}
 }
 
 string client::recv() {
-	return "";
+	int received = ::recv(fd, &buffer, DATA_SIZE, 0);
+	if (received == 0) {
+		throw connection_closed();
+	} else if (received == -1) {
+		throw recv_err();
+	}
+	return string(buffer);
 }
--- src/client/client.h
+++ src/client/client.h
@@ -1,8 +1,27 @@
 #pragma once
 
 #include "../lib/socket.h"
+#include <exception>
 
 #define DATA_SIZE 1000
+
+class send_err : public exception {
+	virtual const char * what() {
+		return "failed sending data";
+	}
+};
+
+class connection_closed : public exception {
+	virtual const char * what() {
+		return "remote has closed connection";
+	}
+};
+
+class recv_err : public exception {
+	virtual const char * what() {
+		return "remote has closed connection";
+	}
+};
 
 class client : connection {
 	char buffer[DATA_SIZE];
