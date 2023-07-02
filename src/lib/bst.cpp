27f186fb9da617926bfc0d66de7fc6156a639bb6
Urmas Rist <urmas@urist.ee>
2023-07-02 18:28:05 +0300

    implement bst::insert

--- src/lib/bst.cpp
+++ src/lib/bst.cpp
@@ -1,5 +1,9 @@
 #include "bst.h"
 
+bst::bst(int number) {
+	key = number;
+}
+
 bool bst::find(int key) {
 	if (this->key == key)
 		return true;
@@ -10,10 +14,31 @@ bool bst::find(int key) {
 	return false;
 }
 
+// TODO: Think about empty tree insertions
 void bst::insert(int key) {
-
+	if (this->key == key)
+		throw bst_exception("key already exists");
+	bst *node = this;
+	bst *previous_node;
+	while (node != NULL) {
+		previous_node = node;		
+		if (node->key == key)
+			throw bst_exception("key already exists");
+		if (node->key > key)
+			node = node->left.get();
+		else
+			node = node->right.get();
+	}
+	if (previous_node->key > key)
+		previous_node->left.reset(new bst(key));
+	else 
+		previous_node->right.reset(new bst(key));
 }
 
+// TODO: Think about deleting last node
 void bst::del(int key) {
-
+	if (this->key == key && !left && !right) {
+		// Server should handle this
+		throw last_node();
+	}
 }
--- src/lib/bst.h
+++ src/lib/bst.h
@@ -1,14 +1,22 @@
+#include <exception>
 #include <memory>
 #include <stdexcept>
 
 class bst_exception : public std::runtime_error {
+	public:
 	bst_exception(const std::string &msg) : std::runtime_error(msg) {};
+};
+
+class last_node : public std::exception {
+
 };
 
 struct bst {
 	int key;
 	std::shared_ptr<bst> left;
 	std::shared_ptr<bst> right;
+	bst() {};
+	bst(int number);
 	bool find(int key);
 	void insert(int key);
 	void del(int key);
