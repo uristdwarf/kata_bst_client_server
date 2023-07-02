7fbfdd3a3dee22594fae41c92bd8b9d53ea960cc
Urmas Rist <urmas@urist.ee>
2023-07-02 17:56:35 +0300

    implement bst::find

--- src/lib/bst.cpp
+++ src/lib/bst.cpp
@@ -1,6 +1,12 @@
 #include "bst.h"
 
 bool bst::find(int key) {
+	if (this->key == key)
+		return true;
+	else if (left && this->key > key)
+		return left->find(key);
+	else if (right && this->key < key)
+		return right->find(key);
 	return false;
 }
 
